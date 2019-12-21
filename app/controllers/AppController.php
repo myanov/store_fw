bf5f2a4b42c52c96aa5c9946cd39a6ab6702a88a
Maksim <m.yanov1998@gmail.com>
2019-12-21 11:37:41 +0700

    Added a menu widget

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -8,6 +8,7 @@ use app\models\AppModel;
 use app\widgets\currency\Currency;
 use ishop\App;
 use ishop\base\Controller;
+use ishop\Cache;
 
 class AppController extends Controller
 {
@@ -17,5 +18,17 @@ class AppController extends Controller
         new AppModel();
         App::$app->setProperty('currencies', Currency::getCurrencies());
         App::$app->setProperty('currency', Currency::getCurrency(App::$app->getProperty('currencies')));
+        App::$app->setProperty('cats', self::cacheCategory());
+    }
+
+    private static function cacheCategory()
+    {
+        $cache = Cache::instance();
+        $cats = $cache->get('cats');
+        if(!$cats) {
+            $cats = \R::getAssoc("SELECT * FROM category");
+            $cache->set('cats', $cats);
+        }
+        return $cats;
     }
 }
